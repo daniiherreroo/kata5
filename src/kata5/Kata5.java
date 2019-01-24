8b1e9f95a3ebbd994f6b0b18516e70ffed0c1e08
Juan <Usuario@DESKTOP-72S09VV>
2019-01-24 16:47:05 +0000

    Kata5 versión 3

--- src/kata5/Kata5.java
+++ src/kata5/Kata5.java
@@ -1,5 +1,9 @@
 package kata5;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -8,8 +12,8 @@ import java.sql.ResultSet;
 
 public class Kata5 {
 
-    public static void main(String[] args) throws SQLException {
-        createTableEmail();
+    public static void main(String[] args) throws SQLException, FileNotFoundException {
+        addRegisters();
     }
     
     private static Connection connection(){
@@ -24,13 +28,12 @@ public class Kata5 {
         return con;
     }
     
-    public static void createTableEmail() throws SQLException{
-        String sql = "create table EMAIL ('Id' integer primary key autoincrement, 'Mail' text not null);";
-        
+    public static void addRegisters() throws SQLException, FileNotFoundException{        
         try{
             Connection con = connection();
             Statement stat = con.createStatement();
-            ResultSet rs = stat.executeQuery(sql);
+            BufferedReader reader = new BufferedReader(new FileReader(new File("email.txt")));
+            System.out.println(stat.executeQuery("select count(*) from EMAIL").getInt(1));
         }catch (SQLException e){
             System.out.println(e.getMessage());
         }
